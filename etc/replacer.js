37df71052ff8b1c42bf877c9e4d23122cc53c152
ystk_skm <sakamoto@liberty-techonology.biz>
2024-11-22 17:31:19 +0900

    basic command

--- etc/replacer.js
+++ etc/replacer.js
@@ -17,7 +17,7 @@
     case 'relativize':
       // parcel build で絶対パスになったものを相対パスに置き換える
       // 複数ファイルを同時に指定可能
-      if(argv[1] == '-a') {
+      if(argv[0] == '-a') {
         return Op_multi(distdir, Op_relativize);
       } else {
         return Op_relativize(distdir, argv).then(()=>{
@@ -40,6 +40,7 @@
         return; // 対象は HTML のみ
       }
       const filepath = path.resolve(srcdir, file);
+      console.log(`operating filepath: ${filepath}`);
       fileWhen = fileWhen.then(()=>{
         return fs.readFileSync(filepath)
       }).then(buf=>{
@@ -53,6 +54,7 @@
   }
   
   async function Op_multi(srcdir, proc, options) {
+    console.log(`(replacer.js) searching directory: ${srcdir}`);
     return Promise.resolve().then(()=>{
       return fs.readdirSync(srcdir);
     }).then(a=>{
